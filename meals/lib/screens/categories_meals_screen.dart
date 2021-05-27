b08928225191e193c5652c79690fe43d49688cb2
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-05-26 21:49:52 -0300

    Ajuste na exibição de dados das categorias
    - Adicionados dados mockados para representar refeições
    - Criado model de refeição
    - Ajuste na tela de categoria para exibir as refeições da categoria

--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS
+        .where((element) => element.categories.contains(category.id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Receitas'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Receitas por categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
