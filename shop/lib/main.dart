88df997bf30985f0566fac07991ab3ac448b85a4
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-06-18 16:08:27 -0300

    Criada tela de detalhes do produto

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/product_detail_screen.dart';
 import 'package:shop/views/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -11,9 +13,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => new ProductDetailScreen(),
+      },
     );
   }
 }
